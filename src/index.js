700b82af75a30f92ffacc891412f89517fbc987c
Francis Albert Ilacad <filacad22@gmail.com>
2022-08-10 23:04:57 +0800

    fixed all bugs

--- src/index.js
+++ src/index.js
@@ -5,11 +5,4 @@ import './index.css';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<ContextProvider><App /></ContextProvider>, document.getElementById('root'));
